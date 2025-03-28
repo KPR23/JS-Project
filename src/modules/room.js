ac2db4d751401cdf54c882d5ac29b8fc59b59a06
KPR23 <zabluka_aehit@students.vizja.pl>
2025-03-28 08:57:00 +0100

    ES6

--- src/modules/room.js
+++ src/modules/room.js
@@ -1,17 +1,19 @@
-function Room(number, type) {
-  this.number = number;
-  this.type = type;
-  this.isAvailable = true;
+class Room {
+  constructor(number, type) {
+    this.number = number;
+    this.type = type;
+    this.isAvailable = true;
+  }
+
+  book() {
+    this.isAvailable = false;
+    return `Room ${this.number} has been booked`;
+  }
+
+  checkOut() {
+    this.isAvailable = true;
+    return `Room ${this.number} has been checked out`;
+  }
 }
-
-Room.prototype.book = function () {
-  this.isAvailable = false;
-  return `Room ${this.number} has been booked`;
-};
-
-Room.prototype.checkOut = function () {
-  this.isAvailable = true;
-  return `Room ${this.number} has been checked out`;
-};
 
 module.exports = Room;
